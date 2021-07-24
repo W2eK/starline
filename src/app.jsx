e0d01318dc5094bc057a64b6dd3a04a7f9737ea1
Vladimir Osokin <osokin.home@gmail.com>
2021-07-24 22:21:32 +0500

    start blank cra-project

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Container from '@material-ui/core/Container';
+
+function App() {
+  return <Container maxWidth="xs">Hello, App!</Container>;
+}
+
+export default App;
